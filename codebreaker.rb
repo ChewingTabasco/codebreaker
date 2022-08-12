c5183647a554e5da616ef4e8d83e89666e5fd3b7
ChewingTabasco <emerickjustice@gmail.com>
2022-08-12 09:52:59 -0400

    Add Maker method for creating 4 digit code

--- codebreaker.rb
+++ codebreaker.rb
@@ -1,6 +1,12 @@
 module Maker
+  attr_reader :maker_code
   def generate_random_code
     random_code = [(1 + rand(6)), (1 + rand(6)), (1 + rand(6)), (1 + rand(6))]
+  end
+
+  def create_code
+    puts 'Maker, create a 4 digit code (1-6).'
+    @maker_code = gets.chomp.split('')
   end
 end
 
@@ -9,3 +15,5 @@ class Game
 end
 
 p Game.generate_random_code
+Game.create_code
+p Game.maker_code
